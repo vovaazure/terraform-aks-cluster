2d20878ca76d64b1a3366a992f1d0b2dbc5cd75e
vovaazure <vovaazure@gmail.com>
2022-11-07 18:18:33 +0400

    Planed

--- 01-main.tf
+++ 01-main.tf
@@ -18,7 +18,7 @@ terraform {
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "~> 1.0"
+      version = "~> 2.0"
     }
     random = {
       source  = "hashicorp/random"
@@ -48,4 +48,3 @@ provider "azurerm" {
 resource "random_pet" "aksrandom" {
 
 }
-
--- 05-log-analytics-workspace.tf
+++ 05-log-analytics-workspace.tf
@@ -1,5 +1,5 @@
 resource "azurerm_log_analytics_workspace" "insights" {
-  name                = "logs-${random_pet.aksrandom}"
+  name                = "logs-${random_pet.aksrandom.id}"
   location            = azurerm_resource_group.aks_rg.location
   resource_group_name = azurerm_resource_group.aks_rg.name
   sku                 = "PerGB2018"
